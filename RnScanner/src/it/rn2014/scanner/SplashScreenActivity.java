892592dba9fc5de7ea0d225064313ac0d793ba53
Nicola Corti <corti.nico@gmail.com>
2014-07-12 01:31:54 +0200

    Added Splash screen support

---
+++ RnScanner/src/it/rn2014/scanner/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package it.rn2014.scanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+ 
+public class SplashScreenActivity extends Activity {
+ 
+    private static int SPLASH_TIMER = 4000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        // Handler che avvia la nuova activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
