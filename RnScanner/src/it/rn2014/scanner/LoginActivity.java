e7481eaceb5e53beb81f539289e773dfb027adec
Nicola Corti <corti.nico@gmail.com>
2014-07-13 12:20:06 +0200

    Splash screen fix

--- RnScanner/src/it/rn2014/scanner/LoginActivity.java
+++ RnScanner/src/it/rn2014/scanner/LoginActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
