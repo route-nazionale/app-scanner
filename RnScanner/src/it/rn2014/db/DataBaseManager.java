a4bb7eec8c2529e5252363ff2d6c85c08e6d242b
Nicola Corti <corti.nico@gmail.com>
2014-07-24 02:35:12 +0200

    General clean and improvement

--- RnScanner/src/it/rn2014/db/DataBaseManager.java
+++ RnScanner/src/it/rn2014/db/DataBaseManager.java
@@ -11,7 +11,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
-import android.util.Log;
 
 /**
  * @author Luca
