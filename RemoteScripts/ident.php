7eaffdec7386f28b0f6eb4029c70a38717bfb0c6
Nicola Corti <corti.nico@gmail.com>
2014-07-25 22:03:09 +0200

    Added identify script

---
+++ RemoteScripts/ident.php
@@ -0,0 +1,55 @@
+<?php
+	
+	$mysql_host = '172.16.10.151';
+	$mysql_db = 'varchi';
+	$mysql_user = 'varchi';
+	$mysql_password = '';
+	
+	if (!isset($_POST['search']) || !isset($_POST['cu']) || !isset($_POST['date'])){
+		http_response_code(400);
+		return;
+	}
+	$search = $_POST['search'];
+	$cu = $_POST['cu'];
+	$date = $_POST['date'];
+	
+	/* TODO 
+	 * Autenticazione
+	 */
+	
+// 	class Risultato {
+// 		public $nome = "";
+// 		public $cognome = "";
+// 		public $data = "";
+// 	}
+	
+	$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db);
+
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		http_response_code(500);
+		return;
+	} else {
+	
+		$sql = "SELECT * FROM `persone`,`date` WHERE persone.codiceUnivoco = date.cu AND   `codiceUnivoco` = '" . $_POST['search'] . "'"; 
+		$result = mysqli_query($con,$sql);
+		$size = mysqli_num_rows($result);
+		
+		if ($size == 1){
+			$row = mysqli_fetch_array($result);
+
+			$json->status = "found";
+			$json->nome = $row['nome'];
+			$json->cognome = $row['cognome'];
+			$json->data = $row['datanascita'];
+			
+			
+		} else {
+			$json->status = "notfound";
+		} 
+			
+		header('Content-Type: application/json');
+		echo json_encode($json);
+		mysqli_close($con);
+	}
+?>
